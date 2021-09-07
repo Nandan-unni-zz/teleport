c959a0b7dbae367b755c47c5f08d7172adf0759e
Nandan-unni <asnqln@gmail.com>
2021-09-07 10:41:29 +0530

    highlight current period

--- S5/assets/app.js
+++ S5/assets/app.js
@@ -1,3 +1,39 @@
+const time = new Date();
+
+const isCurrentPeriod = (subIndex) => {
+  const hour = time.getHours();
+  const min = time.getMinutes();
+  // 9:00 to 10: 30 : 0
+  if (
+    hour >= 9 &&
+    (hour < 10 || (hour === 10 && min <= 30)) &&
+    subIndex === 0
+  ) {
+    return true;
+  }
+  // 10:30 to 12: 30 : 1
+  else if (
+    (hour > 10 || (hour === 10 && min >= 30)) &&
+    (hour < 12 || (hour === 12 && min <= 30)) &&
+    subIndex === 1
+  ) {
+    return true;
+  }
+  // 12:30 to 15: 00 : 2
+  else if (
+    (hour > 12 || (hour === 12 && min >= 30)) &&
+    hour <= 15 &&
+    subIndex === 2
+  ) {
+    return true;
+  }
+  // 15:00 to 16: 00 : 3
+  else if (hour >= 15 && hour <= 16 && subIndex === 3) {
+    return true;
+  }
+  return false;
+};
+
 const classroomBlock = (data, subCode) => `
   <a class="nav" href="${data?.classroomLinks[subCode]}" rel="noopener noreferrer">
     <abbr title="${data?.desc[subCode]}">
@@ -6,18 +42,22 @@ const classroomBlock = (data, subCode) => `
   </a>
 `;
 
-const subjectBlock = (data, day) => `
+const subjectBlock = (data, day, dayIndex) => `
   <section class="day">
     <p class="block">${day}</p>
     ${data?.timetable[day]
       .map(
-        (subCode) => `
+        (subCode, subIndex) => `
       <a
         class="block"
         href="${data?.meetLinks[subCode]}"
         rel="noopener noreferrer"
       >
-        <abbr title="${data?.desc[subCode]}">
+        <abbr class="${
+          dayIndex === time.getDay() - 1 &&
+          isCurrentPeriod(subIndex) &&
+          "activeSub"
+        }" title="${data?.desc[subCode]}">
           ${subCode}
         </abbr>
       </a>`
@@ -36,8 +76,32 @@ const loadDataToHTML = async () => {
     .reduce((a, b) => a + b);
 
   document.getElementById("timetable").innerHTML = Object.keys(data?.timetable)
-    .map((day) => subjectBlock(data, day))
+    .map((day, dayIndex) => subjectBlock(data, day, dayIndex))
     .reduce((a, b) => a + b);
+
+  const timeStr = `${time.getDate()} ${
+    [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ][time.getMonth()]
+  } ${time.getFullYear()} &nbsp; <span> ● </span> &nbsp; ${
+    time.getHours() < 10 ? "0" + time.getHours() : time.getHours()
+  }: ${
+    time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()
+  } &nbsp; <span> ● </span> &nbsp; ${
+    ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"][time.getDay()]
+  }`;
+  document.getElementById("time").innerHTML = timeStr;
 };
 
 loadDataToHTML();
